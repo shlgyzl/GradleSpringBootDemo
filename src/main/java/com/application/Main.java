87cc0e52a79198d2db3332245958b512b0f05531
yanghaiyong <925418240@qq.com>
2019-10-31 18:46:48 +0800

    创建最新的Gradle+SpringBoot项目

--- src/main/java/com/application/Main.java
+++ src/main/java/com/application/Main.java
@@ -2,11 +2,20 @@ package com.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Main.class);
         springApplication.run(args);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Void> index(){
+        return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/application/config/DataSourceConfiguration.java
@@ -0,0 +1,7 @@
+package com.application.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfiguration {
+}
