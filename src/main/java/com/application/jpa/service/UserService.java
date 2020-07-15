251cb39e11ad7a741ce230f6075d427f737830db
yanghaiyong1993 <38939501+yanghaiyong1993@users.noreply.github.com>
2020-07-15 20:05:46 +0800

    添加动态数据源

--- src/main/java/com/application/jpa/service/UserService.java
+++ src/main/java/com/application/jpa/service/UserService.java
@@ -1,19 +1,22 @@
 package com.application.jpa.service;
 
-import com.application.jpa.domain.QUser;
-import com.application.jpa.domain.User;
+import com.application.jpa.domain.*;
 import com.application.jpa.repository.UserRepository;
-import com.querydsl.core.BooleanBuilder;
+import com.application.jpa.service.dto.BookDTO;
+import com.querydsl.core.QueryModifiers;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
-import java.util.Objects;
+import javax.persistence.EntityManager;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -21,6 +24,7 @@ import java.util.Objects;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final EntityManager entityManager;
 
     @Transactional
     public User job(User user) {
@@ -35,13 +39,44 @@ public class UserService {
     }
 
     public Page<User> findAll(User user, Pageable pageable) {
-        BooleanBuilder builder = new BooleanBuilder();
+        JPAQuery<BookDTO> jpaQuery = new JPAQuery<BookDTO>(entityManager);
+        JPAQuery<BookDTO> clone = jpaQuery.clone(entityManager);
+        QUser qUser = QUser.user;
+        QDefectType qDefectType = QDefectType.defectType;
+        QAttachment qAttachment = QAttachment.attachment;
+        QRole qRole = QRole.role;
+        QAuthority qAuthority = QAuthority.authority;
+
+        JPAQuery<BookDTO> query = jpaQuery
+                .select(Projections.constructor(BookDTO.class, qUser.login, qUser.imageUrl, qAuthority.id.countDistinct()).skipNulls())
+                .from(qUser)
+                .join(qRole).on(qRole.id.eq(qUser.roles.any().id))
+                .join(qAuthority).on(qAuthority.id.eq(qRole.authorities.any().id))
+                .groupBy(qUser.login, qUser.imageUrl)
+                .orderBy(qUser.login.asc(), qUser.imageUrl.asc())
+                .restrict(new QueryModifiers(10L, 0L));
+        List<BookDTO> fetch = query.distinct().fetch();
+        int size = query
+                .restrict(QueryModifiers.EMPTY)
+                .fetch().size();
+
+        PageImpl<BookDTO> bookDTOS = new PageImpl<>(fetch, pageable, size);
+        System.out.println(bookDTOS);
+
+
+
+
+
+
+
+        /*BooleanBuilder builder = new BooleanBuilder();
         if (Objects.nonNull(user)) {
             QUser qUser = QUser.user;
             if (StringUtils.hasText(user.getLogin())) {
                 builder.and(qUser.login.containsIgnoreCase(user.getLogin()));
             }
         }
-        return userRepository.findAll(builder, pageable);
+        return userRepository.findAll(builder, pageable);*/
+        return null;
     }
 }
---
+++ src/main/java/com/application/jpa/service/dto/BookDTO.java
@@ -0,0 +1,26 @@
+package com.application.jpa.service.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author yanghaiyong
+ * 2020/7/13-20:19
+ */
+@Data
+public class BookDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String login;
+    private String name;
+    private Long count;
+
+    @QueryProjection
+    public BookDTO(String login, String name, Long count) {
+        this.login = login;
+        this.name = name;
+        this.count = count;
+    }
+}
